7e3f046cc2b72980a3169f7ebc6bbd15283ce11d
Sergey Burykin <xtreding@gmail.com>
2020-10-19 13:38:29 +0300

    always return last-modified

--- backend/middleware/content_changes.go
+++ backend/middleware/content_changes.go
@@ -16,17 +16,17 @@ const (
 //IfModifiedSince check if content wasn't modified -> return http 304
 func IfModifiedSince(main gin.HandlerFunc, getLastModified func() (*time.Time, error)) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ifModifiedSinceStr := c.GetHeader(ifModifiedSinceHeader)
-		if ifModifiedSinceStr != "" {
-			if ifModifiedSince, err := time.Parse(storages.LastUpdatedLayout, ifModifiedSinceStr); err != nil {
-				logging.Warnf("Error parsing [%s:%s] header with [%s] layout: %v", ifModifiedSinceHeader, ifModifiedSinceStr, storages.LastUpdatedLayout, err)
-			} else {
-				lastModified, err := getLastModified()
-				if err != nil {
-					logging.Warn(err)
-				} else {
-					c.Writer.Header().Add(lastModifiedHeader, lastModified.Format(storages.LastUpdatedLayout))
+		lastModified, err := getLastModified()
+		if err != nil {
+			logging.Warn(err)
+		} else {
+			c.Writer.Header().Add(lastModifiedHeader, lastModified.Format(storages.LastUpdatedLayout))
 
+			ifModifiedSinceStr := c.GetHeader(ifModifiedSinceHeader)
+			if ifModifiedSinceStr != "" {
+				if ifModifiedSince, err := time.Parse(storages.LastUpdatedLayout, ifModifiedSinceStr); err != nil {
+					logging.Warnf("Error parsing [%s:%s] header with [%s] layout: %v", ifModifiedSinceHeader, ifModifiedSinceStr, storages.LastUpdatedLayout, err)
+				} else {
 					if !lastModified.After(ifModifiedSince) {
 						c.Status(http.StatusNotModified)
 						return
@@ -35,7 +35,6 @@ func IfModifiedSince(main gin.HandlerFunc, getLastModified func() (*time.Time, e
 			}
 		}
 
-		//only if lastModified is after ifModifiedSince
 		main(c)
 	}
 }
