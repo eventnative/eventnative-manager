b7a470221a899135169fcc4014b0f3f67db680c2
Sergey Burykin <xtreding@gmail.com>
2020-11-13 17:46:44 +0300

    added url parsing in check origin

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -2,16 +2,21 @@ package middleware
 
 import (
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func Cors(h http.Handler, allowedDomainSuffix string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		reqOrigin := r.Header.Get("Origin")
-		reqOriginWithoutPort := strings.Split(reqOrigin, ":")[0]
-		if strings.HasSuffix(reqOriginWithoutPort, allowedDomainSuffix) {
-			w.Header().Add("Access-Control-Allow-Origin", reqOriginWithoutPort)
+		u, err := url.Parse(reqOrigin)
+		if err == nil {
+			reqOriginWithoutPort := strings.Split(u.Host, ":")[0]
+			if strings.HasSuffix(reqOriginWithoutPort, allowedDomainSuffix) {
+				w.Header().Add("Access-Control-Allow-Origin", reqOrigin)
+			}
 		}
+
 		w.Header().Add("Access-Control-Max-Age", "86400")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Host, X-Client-Auth")
